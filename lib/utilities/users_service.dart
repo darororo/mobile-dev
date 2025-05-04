3de213a5b5b56dbebe7beeeb33b240db5361d46c
darororo <daroyem99@gmail.com>
2025-05-04 17:16:08 +0700

    finish CRUD for users

--- lib/utilities/users_service.dart
+++ lib/utilities/users_service.dart
@@ -1,25 +1,46 @@
-// TODO-2: Import http and dart:convert
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class UsersService {
   static final UsersService _singleton = UsersService._internal();
   UsersService._internal();
   static UsersService get instance => _singleton;
 
-  // TODO-3: Add base URL
+  final String baseUrl = "https://6817396926a599ae7c39aeae.mockapi.io";
 
   Future<List<dynamic>> getUsers() async {
-    // TODO-4: Fetch all users
+    Response res = await get(Uri.parse('$baseUrl/users'));
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    } else {
+      throw "Unable to retrieve contacts";
+    }
   }
 
   Future<dynamic> updateUser(String id, Map<String, dynamic> data) async {
-    // TODO-5: Update a user
+    Response res = await patch(Uri.parse('$baseUrl/users/$id'), body: data);
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    } else {
+      throw "Unable to update contact";
+    }
   }
 
   Future<dynamic> createUser(Map<String, dynamic> data) async {
-    // TODO-6: Create new user
+    Response res = await post(Uri.parse('$baseUrl/users'), body: data);
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    } else {
+      throw "Unable to create contact";
+    }
   }
 
   Future<dynamic> deleteUser(String id) async {
-    // TODO-7: Delete a user
+    Response res = await delete(Uri.parse('$baseUrl/users/$id'));
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    } else {
+      throw "Unable to delete contact";
+    }
   }
 }
